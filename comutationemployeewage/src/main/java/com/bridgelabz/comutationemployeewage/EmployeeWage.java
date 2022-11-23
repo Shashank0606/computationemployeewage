6b783b9fac594d179d9c20971c5c9941839a485e
Shashank0606 <shashankrathore606@gmail.com>
2022-11-23 17:43:02 +0530

    ADD: Present Absent Employee Program

---
+++ comutationemployeewage/src/main/java/com/bridgelabz/comutationemployeewage/EmployeeWage.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.comutationemployeewage;
+
+public class EmployeeWage {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+        int employeeCheck = (int) Math.floor(Math.random() * 10) % 2;
+
+        if (employeeCheck == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+    }
+
+}
---
+++ comutationemployeewage/src/test/java/com/bridgelabz/comutationemployeewage/ComutationemployeewageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.comutationemployeewage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComutationemployeewageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
