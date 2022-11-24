d2bfd9d610061bba2a86c03574e27836f4d994bf
Shashank0606 <shashankrathore606@gmail.com>
2022-11-24 09:14:47 +0530

    REFACTOR: ClassMethodComputeEmployeeWage

--- comutationemployeewage/src/main/java/com/bridgelabz/comutationemployeewage/EmployeeWage.java
+++ comutationemployeewage/src/main/java/com/bridgelabz/comutationemployeewage/EmployeeWage.java
@@ -1,35 +1,37 @@
 package com.bridgelabz.comutationemployeewage;
 
 public class EmployeeWage {
+    static int WagePerHour = 20;
+    static int FullTimeHour = 8;
+    static int PartTimeHour = 4;
 
     public static void main(String[] args) {
-        int wagePerHour = 20;
-        int fullTimeHour = 8;
-        int partTimeHour = 4;
         int totalWorkingDays = 0;
         int totalHour = 0;
         int totalDailyWage = 0;
-
         while (totalHour < 100 && totalWorkingDays < 20) {
             int employeeCheck = (int) (Math.random() * ((2 + 0) + 1));
             totalWorkingDays++;
 
             switch (employeeCheck) {
                 case 2:
-                    totalHour = totalHour + fullTimeHour;
-                    totalDailyWage = totalDailyWage + fullTimeHour * wagePerHour;
+                    totalHour = totalHour + FullTimeHour;
+                    totalDailyWage = totalDailyWage + FullTimeHour * WagePerHour;
                     System.out.println("Employee is present full time: " + totalDailyWage);
                     break;
                 case 1:
-                    totalHour = totalHour + partTimeHour;
-                    totalDailyWage = totalDailyWage + partTimeHour * wagePerHour;
+                    totalHour = totalHour + PartTimeHour;
+                    totalDailyWage = totalDailyWage + PartTimeHour * WagePerHour;
                     System.out.println("Employee is present half time: " + totalDailyWage);
                     break;
                 default:
                     System.out.println("Employee is absent: " + totalDailyWage);
+
+                    System.out.println("Working Days is: " + totalWorkingDays + " Hours: " + totalHour);
+
+                    System.out.println("Employee total wage for month is: " + totalDailyWage);
             }
-            System.out.println("Working Days is: " + totalWorkingDays + " Hours: " + totalHour);
         }
-        System.out.println("Employee total wage for month is: " + totalDailyWage);
     }
+
 }
