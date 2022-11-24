294609b43e9e990e9a101aeffffdb589f8561d7e
Shashank0606 <shashankrathore606@gmail.com>
2022-11-24 08:03:50 +0530

    REFACTOR: Daily Employee Wage

--- comutationemployeewage/src/main/java/com/bridgelabz/comutationemployeewage/EmployeeWage.java
+++ comutationemployeewage/src/main/java/com/bridgelabz/comutationemployeewage/EmployeeWage.java
@@ -4,13 +4,18 @@ public class EmployeeWage {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
-        int employeeCheck = (int) Math.floor(Math.random() * 10) % 2;
 
-        if (employeeCheck == 1) {
-            System.out.println("Employee is Present");
+        int wagePerHour = 20;
+        int fullDayHour = 8;
+        int dailyWage = 0;
+        int employeeCheck1 = (int) Math.floor(Math.random() * 10) % 2;
+
+        if (employeeCheck1 == 1) {
+            dailyWage = fullDayHour * wagePerHour;
+            System.out.println("Employee is present");
         } else {
-            System.out.println("Employee is Absent");
+            System.out.println("Employee is absent");
         }
+        System.out.println("Total wage of employee is: " + dailyWage);
     }
-
 }
