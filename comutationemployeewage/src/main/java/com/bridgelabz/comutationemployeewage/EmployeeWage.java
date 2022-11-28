f3ae9297288db06d223766f8e12948b692bb9fcb
Shashank0606 <shashankrathore606@gmail.com>
2022-11-28 08:09:26 +0530

    REFACTOR: Employee Wage for Multiple Companies

--- comutationemployeewage/src/main/java/com/bridgelabz/comutationemployeewage/EmployeeWage.java
+++ comutationemployeewage/src/main/java/com/bridgelabz/comutationemployeewage/EmployeeWage.java
@@ -3,52 +3,64 @@ package com.bridgelabz.comutationemployeewage;
 import java.util.Random;
 
 public class EmployeeWage {
-    final static int FullTimeWorker = 1;
-    final static int PartTimeWorker = 2;
+    private String company;
+    static int EMP_RATE_PER_HOUR;
+    static int WORKING_DAYS_PER_MONTH;
+    static int MAX_WORKING_HOURS;
+    private int totalEmpWage;
 
-    public static void program(String company, int EmpRatePerHour, int TotalnoWorkingDay,
-            int EmpTotalHours) {
-        int totalworkingdays = 0;
-        int empHours = 0;
-        int empWage = 0;
-        int Totalemphrs = 0;
+    public EmployeeWage(String company, int EMP_RATE_PER_HOUR, int workingDaysPerMonth, int maxWorkingHours) {
+        this.company = company;
+        this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
+        this.WORKING_DAYS_PER_MONTH = workingDaysPerMonth;
+        this.MAX_WORKING_HOURS = maxWorkingHours;
+    }
 
-        int Total_emp_wage = 0;
-        while (Totalemphrs <= EmpTotalHours && totalworkingdays <= TotalnoWorkingDay) {
-            totalworkingdays++;
-            Random random = new Random();
+    public void companyWage() {
+        System.out.printf("%s Employee Wage details: ", company).println();
 
-            int empCheck = random.nextInt(2);
+        int empWage = 0, totalWage = 0, workingHours = 0, empHours = 0;
+        Random random = new Random();
 
+        for (int day = 1; day <= WORKING_DAYS_PER_MONTH && workingHours < MAX_WORKING_HOURS; day++) {
+
+            int empCheck = random.nextInt(3);
             switch (empCheck) {
-                case FullTimeWorker:
+                case 1:
+                    System.out.printf("Day %d: Employee is Present for full day.", day).println();
                     empHours = 8;
-
                     break;
-                case PartTimeWorker:
+                case 2:
+                    System.out.printf("Day %d: Employee worked for half day.", day).println();
                     empHours = 4;
-
                     break;
                 default:
+                    System.out.printf("Day %d: Employee is Absent.", day).println();
                     empHours = 0;
+                    break;
             }
-            Totalemphrs = Totalemphrs + empHours;
-            empWage = empHours * EmpRatePerHour;
-            System.out.println("Daily emp wage is" + empWage);
-            Total_emp_wage = empWage + Totalemphrs;
-            Total_emp_wage = Total_emp_wage * TotalnoWorkingDay;
-
+            empWage = empHours * EMP_RATE_PER_HOUR;
+            System.out.printf("Worked:" + empHours + "hours, Wage:" + empWage).println("\n");
+            totalWage += empWage;
+            workingHours += empHours;
         }
-        System.out.println("The Monthly Wage of Employee of " + company + " is " + Total_emp_wage);
+        totalEmpWage = workingHours * EMP_RATE_PER_HOUR;
+        System.out.printf("The Total wage of " + company + " for this month:  " + totalWage).println();
+        System.out.printf("Total hours of working for " + company + ": " + workingHours).println();
+        System.out.println();
+    }
+
+    public String toString() {
+        return "Total wage  for the Company " + company + " is: " + totalEmpWage;
     }
 
     public static void main(String[] args) {
-
-        program("Vedanta", 20, 20, 200);
-
-        program("TCS", 30, 30, 300);
-
-        program("Wipro", 50, 20, 220);
-
+        System.out.println("Welcome to Employee Wage Computation. \n");
+        EmployeeWage company1 = new EmployeeWage("Vedanta", 20, 20, 100);
+        company1.companyWage();
+        EmployeeWage company2 = new EmployeeWage("TCS", 15, 26, 200);
+        company2.companyWage();
+        System.out.println(company1);
+        System.out.println(company2);
     }
 }
